d3d7d3b6378af2022b71b7aac9a6335914228f49
Travis Horn <travis@travishorn.com>
2021-01-14 14:25:43 -0600

    Generate magnet URLs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+// Functions are in separate files. Require them.
 const lm = require("./listMovies");
 
+// The base URL for the YTS API. As of 2021-01-14 via https://yts.mx/api
 const baseUrl = "https://yts.mx/api/v2";
 
+/*
+ * Create a listMovies function that always uses the specified base URL
+ * Can be called via listMovies(params) where params is an object following
+ * the endpoint parameters at https://yts.mx/api#list_movies
+ */
 const listMovies = async (params) => lm(baseUrl, params);
 
+// Export the composed functions so they can be called like yts.listMovies() etc.
 module.exports = {
   listMovies,
 };
